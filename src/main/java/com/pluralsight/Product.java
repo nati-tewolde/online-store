d13eb2fe8a6688d6497c5dfa9c382caa1a02f1e3
nati-tewolde <natisnewemail@gmail.com>
2025-10-22 21:41:03 -0400

    Add Product class with fields, constructors, and getters

--- src/main/java/com/pluralsight/Product.java
+++ src/main/java/com/pluralsight/Product.java
@@ -1,4 +1,33 @@
 package com.pluralsight;
 
 public class Product {
+    // Apply static method possibly?Ad
+
+    private String sku;
+    private String name;
+    private double price;
+    private String department;
+
+    public Product(String sku, String name, double price, String department) {
+        this.sku = sku;
+        this.name = name;
+        this.price = price;
+        this.department = department;
+    }
+
+    public String getSku() {
+        return sku;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
 }
